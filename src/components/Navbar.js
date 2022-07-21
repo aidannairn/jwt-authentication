e8f98538d219ccb2a40882d3052f7198235c2562
aidannairn <aidan.nairn95@gmail.com>
2022-07-21 16:49:37 +1200

    Use navbar logout button to log the current user out.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,27 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate()
+
+  const handleUserLogout = async () => {
+    try {
+      await axios.delete('/logout')
+      navigate('/', { replace: true })
+    } catch (error) {
+      if (error) console.log(error.response.data)
+    }
+  }
+
   return (
-    <div>Navbar</div>
+    <div id="navbar-container">
+      <h1>DASHBOARD</h1>
+      <div className="nav-btn">
+        Home
+      </div>
+      <div className="nav-btn" onClick={handleUserLogout}>
+        Log Out
+      </div>
+    </div>
   )
 }
 

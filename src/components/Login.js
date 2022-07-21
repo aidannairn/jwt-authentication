d0314be8ceb92f2f908e02daf2aa5cf0d36d1d5b
aidannairn <aidan.nairn95@gmail.com>
2022-07-21 16:34:24 +1200

    User is redirected from login page to dashboard on successful login from form submit.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,44 @@
 import { useState } from 'react'
 import axios from 'axios';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [msg, setMsg] = useState('')
+
+  const navigate = useNavigate()
+
+  const handleInputChange = (e, type) => {
+    if (type === 'email') return setEmail(e.target.value)
+    if (type === 'password') return setPassword(e.target.value)
+  }
+
+  const handleUserLogin = async (e) => {
+    e.preventDefault()
+    try {
+      await axios.put('/login', {
+        email,
+        password
+      })
+      navigate('/dashboard', { replace: true })
+    } catch (error) {
+      if (error) return setMsg(error.response.data)
+    }
+  }
+
   return (
-    <div>Login</div>
+    <div id="auth-container">
+      <div id="login-container">
+        <h1>Login</h1>
+        {msg !== '' && <p>{msg}</p>}
+        <form id="login-form" onSubmit={handleUserLogin}>
+          <input type="text" placeholder='your-email@example.com' value={email} onChange={e => handleInputChange(e, 'email')}/>
+          <input type="password" placeholder='Password' value={password} onChange={e => handleInputChange(e, 'password')}/>
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
   )
 }
 

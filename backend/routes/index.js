2ce9d66f0938b458b2219779a77541c1787c6779
aidannairn <aidan.nairn95@gmail.com>
2022-07-21 15:07:49 +1200

    Add VerifyToken middleware.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 
 const { refreshToken } = require('../controllers/RefreshToken')
+const { verifyToken } = require('../middleware/VerifyToken')
 const { getUsers, signupUser, loginUser, logoutUser } = require('../controllers/Users')
 
 const router = express.Router()
 
-router.get('/users', getUsers)
+router.get('/users', verifyToken, getUsers)
 router.post('/signup', signupUser)
 router.put('/login', loginUser)
 router.get('/token', refreshToken)
